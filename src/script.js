f72e2b08140f1faf66e062bf95d012e3f41f1d27
Kim Pham <kimpham@Kims-MacBook-Pro.local>
2018-07-28 14:32:53 +0700

    top up button

--- src/script.js
+++ src/script.js
@@ -1,94 +1,22 @@
-//images slide show homepage
+// top-up button
 
-$('.slider').each(function () {
-  let $this = $(this);
-  let $group = $this.find('.slide_group');
-  let $slides = $this.find('.slide');
-  let bulletArray = [];
-  let currentIndex = 0;
-  let timeout;
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-  function move(newIndex) {
-    let animateLeft, slideLeft;
-
-    advance();
-
-    if ($group.is(':animated') || currentIndex === newIndex) {
-      return;
-    }
-
-    bulletArray[currentIndex].removeClass('active');
-    bulletArray[newIndex].addClass('active');
-
-    if (newIndex > currentIndex) {
-      slideLeft = '100%';
-      animateLeft = '-100%';
-    } else {
-      slideLeft = '-100%';
-      animateLeft = '100%';
-    }
-
-    $slides.eq(newIndex).css({
-      display: 'block',
-      left: slideLeft
-    });
-    $group.animate({
-      left: animateLeft
-    }, function () {
-      $slides.eq(currentIndex).css({
-        display: 'none'
-      });
-      $slides.eq(newIndex).css({
-        left: 0
-      });
-      $group.css({
-        left: 0
-      });
-      currentIndex = newIndex;
-    });
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("topUpBtn").style.display = "block";
+  } else {
+    document.getElementById("topUpBtn").style.display = "none";
   }
+}
 
-  function advance() {
-    clearTimeout(timeout);
-    timeout = setTimeout(function () {
-      if (currentIndex < ($slides.length - 1)) {
-        move(currentIndex + 1);
-      } else {
-        move(0);
-      }
-    }, 4000);
-  }
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
-  $('.next_btn').on('click', function () {
-    if (currentIndex < ($slides.length - 1)) {
-      move(currentIndex + 1);
-    } else {
-      move(0);
-    }
-  });
-
-  $('.previous_btn').on('click', function () {
-    if (currentIndex !== 0) {
-      move(currentIndex - 1);
-    } else {
-      move(3);
-    }
-  });
-
-  $.each($slides, function (index) {
-    let $button = $('<a class="slide_btn">&bull;</a>');
-
-    if (index === currentIndex) {
-      $button.addClass('active');
-    }
-    $button.on('click', function () {
-      move(index);
-    }).appendTo('.slide_buttons');
-    bulletArray.push($button);
-  });
-
-  advance();
-});
 
 //login - new user
 
